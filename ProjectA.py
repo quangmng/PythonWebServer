f9328cfb11af3f8be6c9f5e4d22aa0646e08abc4
Quang Minh Nguyen <quangng711@outlook.com.au>
2023-04-13 22:34:46 +1000

    Modified some variables... partially worked

--- ProjectA.py
+++ ProjectA.py
@@ -1,5 +1,4 @@
 # import socket module
-# insert < 8 lines of code - remove me before submission.
 from socket import *
 import sys  # Terminate prog.
 import threading  # multithreading
@@ -9,38 +8,6 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = 6789
 serverSocket.bind(("192.168.0.2", serverPort))
 
-def handle_client(conn, addr):
-    global message
-    print(f"[NEW CONNECTION] {addr} connected.")
-    connected = True
-    while connected:
-        try:
-            message = connectionSocket.recv(1024).decode()
-            filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata = f.read()
-            # Send one HTTP header line into socket
-            message = connectionSocket.recvfrom(1024).decode()
-            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-
-            # Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
-                connectionSocket.send("\r\n".encode())
-                connectionSocket.close()
-
-        except IOError:
-            # Send response message for file not found
-            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-            connectionSocket.send("<html><head>404 Not Found</head><body></body></html>\r\n".encode())
-            connectionSocket.close()
-
-        # Close client socket
-        # Fill in start
-        connectionSocket.close()
-        # Fill in end
-    serverSocket.close()
-
 
 def start():
     serverSocket.listen()
@@ -48,10 +15,44 @@ def start():
     while True:
         # Establish the connection
         conn, addr = serverSocket.accept()
-        connectionSocket, addr = serverSocket.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+def handle_client(conn, addr):
+    global message
+
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        try:
+            message = conn.recv(1024).decode()
+            filename = message.split()[1]
+            f = open(filename[1:], 'r')
+            outputdata = f.read()
+            # Send one HTTP header line into socket
+            message = conn.recvfrom(1024).decode()
+            conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+            conn.send("\r\n".encode())
+            # Send the content of the requested file to the client
+            for i in range(0, len(outputdata)):
+                conn.send(outputdata[i].encode())
+                conn.send("\r\n".encode())
+                # Terminate connection
+                conn.close()
+
+        except IOError:
+            # Send response message for file not found
+            conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+            conn.send("<html><head>404 Not Found</head><body></body></html>\r\n".encode())
+            conn.close()
+
+        # Close client socket
+        # Fill in start
+        conn.close()
+        # Fill in end
+    serverSocket.close()
 
 
 print("[STARTING] server is starting...")
