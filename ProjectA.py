f54ecd1c3f71ae837679f7d5b7e5d8d0ddff91a8
Quang Minh Nguyen <quangng711@outlook.com.au>
2023-04-15 11:59:47 +1000

    getting rid of serverSocket.close(), problematic line

--- ProjectA.py
+++ ProjectA.py
@@ -24,36 +24,32 @@ def handle_client(conn, addr):
     global message
 
     print(f"[NEW CONNECTION] {addr} connected.")
-    connected = True
-    while connected:
-        try:
-            message = conn.recv(1024).decode()
-            filename = message.split()[1]
-            f = open(filename[1:], 'r')
-            outputdata = f.read()
-            # Send one HTTP header line into socket
-            message = conn.recvfrom(1024).decode()
-            conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-            conn.send("\r\n".encode())
-            # Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                conn.send(outputdata[i].encode())
-                conn.send("\r\n".encode())
-                # Terminate connection
-                conn.close()
+    try:
+        message = conn.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:], 'r')
+        outputdata = f.read()
 
-        except IOError:
-            # Send response message for file not found
-            conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-            conn.send("<html><head>404 Not Found</head><body></body></html>\r\n".encode())
-            conn.close()
+        # Send one HTTP header line into socket
+        conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        conn.send("\r\n".encode())
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            conn.send(outputdata[i].encode())
+        conn.send("\r\n".encode())
 
-        # Close client socket
-        # Fill in start
+    except IOError:
+        # Send response message for file not found
+        conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        conn.send("<html><title>404 Not Found</title><body><h1>404 Not Found</h1></body></html>\r\n".encode())
         conn.close()
-        # Fill in end
-    serverSocket.close()
 
+    # Close client socket
+    conn.close()
+
+
+
+serverSocket.close()
 
 print("[STARTING] server is starting...")
 start()
